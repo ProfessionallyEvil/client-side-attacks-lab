aafd2b51270eca6d3dc6fc42b87d795b48022e26
Mic <mic@secureideas.com>
2018-05-22 15:50:08 -0400

    Updated vagrant provisioner in copy targets into VM

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -6,4 +6,4 @@ curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install yarn
 
-sudo apt-get install -y nginx
+sudo apt-get install -y nginx tmux vim
