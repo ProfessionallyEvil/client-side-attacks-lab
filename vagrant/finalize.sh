69f2ea7de92de8f356fd04ae3ec6f655505994a8
Mic <mic@secureideas.com>
2018-05-23 09:52:51 -0400

    Added scripts to load node modules and start targets

---
+++ vagrant/finalize.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+
+# Load all package dependencies
+cd /home/vagrant/targets/help.vlab
+yarn install && (setsid yarn start &)
+cd /home/vagrant/targets/www.weakco.vlab
+yarn install && (setsid yarn start &)
+cd /home/vagrant/targets/api.weakco.vlab
+yarn install && (setsid yarn start &)
+cd /home/vagrant/targets/evilhacker.vlab
+yarn install && (setsid yarn start &)
+
+IPADDR="$(ip a | grep eth1 -A 2 | grep inet\ | cut -d " " -f 6 | cut -d "/" -f 1)"
+
+echo $IPADDR    www.weakco.vlab >> /home/vagrant/targets/help.vlab/public/host_entries.txt
+echo $IPADDR    api.weakco.vlab >> /home/vagrant/targets/help.vlab/public/host_entries.txt
+echo $IPADDR    help.vlab >> /home/vagrant/targets/help.vlab/public/host_entries.txt
+echo $IPADDR    evilhacker.vlab >> /home/vagrant/targets/help.vlab/public/host_entries.txt
+
+echo --------------------------
+echo Host-only IP is on: $IPADDR
+echo --------------------------
+echo If you need host file entries, navigate to:
+echo http://$IPADDR:3001/host_entries.txt
